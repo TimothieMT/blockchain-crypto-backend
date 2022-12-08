5806c22a6377fc234a2dda9e79eefedd103e234d
TimothieMT <ttolk89@gmail.com>
2022-12-08 15:43:24 +0100

    init commit

---
+++ src/server.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors'
+import {curry} from "lodash-es";
+
+const app = express()
+app.use(cors())
+const port = 3011;
+
+type Coin = {
+    image: string
+    name: string
+    current_price: number
+    price_change_percentage_24h: number
+}
+
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
+const options = {
+    method: 'GET',
+    url: url,
+    headers: {
+        'Accept-Encoding': 'application/json'
+    }
+};
+const rawCoins: any[] = (await axios.request(options)).data;
+
+const coins: Coin[] = []
+
+rawCoins.forEach((rawCoin: any) => {
+    const coin: Coin = {
+        image: '',
+        name: '',
+        current_price: 0,
+        price_change_percentage_24h: 0
+    }
+    coins.push(coin)
+})
+
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('This is from express.js')
+})
+
+app.get('/crypto', (req: express.Request, res: express.Response) => {
+    res.json(rawCoins)
+})
+
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`)
+});
+
